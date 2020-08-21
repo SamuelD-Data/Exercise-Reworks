a1c5be88978bfbfb3a842b685cbc72f7d3116a57
Samuel Davila <samuelrdavila@gmail.com>
2020-08-21 08:33:39 -0500

    initial commit

---
+++ more_exercises.sql
@@ -0,0 +1,11 @@
+-- How much do the current managers of each department get paid, relative to the average salary for the department? 
+
+
+
+-- Is there any department where the department manager gets paid less than the average salary?
+
+
+
+-- What languages are spoken in Santa Monica?
+
+use world;
