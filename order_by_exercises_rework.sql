e732c856a275a281b18e3b08253b4917586d7e49
Samuel Davila <samuelrdavila@gmail.com>
2020-08-22 12:37:15 -0500

    initial commit

---
+++ order_by_exercises_rework.sql
@@ -0,0 +1,13 @@
+-- Create a new file named order_by_exercises.sql and copy in the contents of your exercise from the previous lesson.
+
+-- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
+
+-- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name and then last name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
+
+-- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by last name and then first name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
+
+-- Write a query to to find all current employees whose last name starts and ends with 'E'. Sort the results by their employee number. Enter a comment with the number of employees returned, the first employee number and their first and last name, and the last employee number with their first and last name.
+
+-- Write a query to to find all current employees whose last name starts and ends with 'E'. Sort the results by their hire date, so that the newest employees are listed first. Enter a comment with the number of employees returned, the name of the newest employee, and the name of the oldest emmployee.
+
+-- Find all previous employees hired in the 90s and born on Christmas. Sort the results so that the oldest employee who was hired last is the first result. Enter a comment with the number of employees returned, the name of the oldest employee who was hired last, and the name of the youngest emmployee who was hired first.
