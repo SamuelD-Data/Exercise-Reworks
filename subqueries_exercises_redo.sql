67a27c66a3e67475ecd002b04fdb59a8840b55f2
Samuel Davila <samuelrdavila@gmail.com>
2020-08-28 13:39:14 -0500

    all questions added

--- subqueries_exercises_redo.sql
+++ subqueries_exercises_redo.sql
@@ -1,2 +1,21 @@
 -- Create a file named subqueries_exercises.sql
 
+-- Find all the current employees with the same hire date as employee 101010 using a sub-query.
+
+-- Find all the titles ever held by all current employees with the first name Aamod.
+
+-- How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
+
+-- Find all the current department managers that are female. List their names in a comment in your code.
+
+-- Find all the employees who currently have a higher salary than the companies overall, historical average salary.
+
+-- How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
+
+--BONUS
+
+-- Find all the department names that currently have female managers.
+
+-- Find the first and last name of the employee with the highest salary.
+
+-- Find the department name that the employee with the highest salary works in.
