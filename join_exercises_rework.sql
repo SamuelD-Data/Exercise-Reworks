a61763347fa1701b3054e7c4727b6bc3519fa342
Samuel Davila <samuelrdavila@gmail.com>
2020-08-27 20:17:51 -0500

    intial commit

---
+++ join_exercises_rework.sql
@@ -0,0 +1,19 @@
+-- Use the employees database.
+
+-- Using the example in the Associative Table Joins section as a guide, write a query that shows each department along with the name of the current manager for that department.
+
+-- Find the name of all departments currently managed by women.
+
+-- Find the current salary of all current managers.
+
+-- Find the number of current employees in each department.
+
+-- Which department has the highest average salary? Hint: Use current not historic information.
+
+-- Who is the highest paid employee in the Marketing department?
+
+-- Which current department manager has the highest salary?
+
+-- Bonus Find the names of all current employees, their department name, and their current manager's name.
+
+-- Bonus Who is the highest paid employee within each department.
