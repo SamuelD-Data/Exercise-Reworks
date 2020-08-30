08eeba94de94856bc35707a9fa55d1dc1ef3e7a5
Samuel Davila <samuelrdavila@gmail.com>
2020-08-30 14:04:06 -0500

    all questions added

--- temporary_tables_redo.sql
+++ temporary_tables_redo.sql
@@ -1,1 +1,13 @@
--- Create a file named temporary_tables.sql to do your work for this exercise.
+-- Using the example from the lesson, re-create the employees_with_departments table.
+
+--  Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
+-- Update the table so that full name column contains the correct data
+-- Remove the first_name and last_name columns from the table.
+
+-- What is another way you could have ended up with this same table?
+
+-- Create a temporary table based on the payment table from the sakila database.
+
+-- Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. For example, 1.99 should become 199.
+
+-- Find out how the current average pay in each department compares to the overall, historical average pay. In order to make the comparison easier, you should use the Z-score for salaries. In terms of salary, what is the best department right now to work for? The worst?Create a file named temporary_tables.sql to do your work for this exercise.
