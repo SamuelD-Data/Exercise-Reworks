c8479ca1a6a6451a7b3834422b7ef5e8eedb873e
Samuel Davila <samuelrdavila@gmail.com>
2020-08-31 12:23:07 -0500

    all questions done except final 2

--- temporary_tables_redo.sql
+++ temporary_tables_redo.sql
@@ -42,4 +42,44 @@ FROM sakila.payment;
 
 -- Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. For example, 1.99 should become 199.
 
--- Find out how the current average pay in each department compares to the overall, historical average pay. In order to make the comparison easier, you should use the Z-score for salaries. In terms of salary, what is the best department right now to work for? The worst?Create a file named temporary_tables.sql to do your work for this exercise.
+ALTER TABLE payment ADD amount_v2 INT(50); -- add new column that will store amounts as cents
+
+UPDATE payment 
+SET amount_v2 = amount * 100; -- populate new column with amount values * 100 to convert to cents
+
+ALTER TABLE payment MODIFY amount_v2 INT(50) AFTER amount; -- move new column next to amount column
+
+ALTER TABLE payment DROP COLUMN amount; -- drop old amount column
+
+ALTER TABLE payment CHANGE amount_v2 amount INT(50); -- change name of new column to match old column
+
+-- Find out how the current average pay in each department compares to the overall, historical average pay. In order to make the comparison easier, you should use the Z-score for salaries. 
+
+create temporary table z_score_V3 as 
+select dept_name, avg(salary) as avg_dept_sal
+from employees.salaries as S
+JOIN employees.dept_emp as DE on DE.emp_no = S.emp_no -- Create temp table that contains each dept with the respective avg. salaries
+JOIN employees.departments as D on D.dept_no = DE.dept_no
+GROUP BY dept_name;
+
+alter table z_score_V3 add total_avg VARCHAR(100); -- add column to temp table that will contain total avg of all salaries
+
+update z_score_V3
+set total_avg = (SELECT AVG(employees.salaries.salary) from employees.salaries); -- populate column w/ total avg of all salaries
+
+alter table z_score_V3 add total_sdev VARCHAR(100); -- add column to temp table that will contain sdev of all salaries
+
+update z_score_V3
+set total_sdev = (SELECT STD(employees.salaries.salary) from employees.salaries); -- populate column w/ sdev of all salaries
+
+alter table z_score_V3 add zscore VARCHAR(100); -- column to table that will contain zscore
+
+update z_score_V3
+set zscore = ((avg_dept_sal) - (total_avg)) / total_sdev; -- populate column with zscores
+
+select dept_name, zscore -- examine results
+from z_score_V3; 
+
+-- In terms of salary, what is the best department right now to work for? 
+
+-- The worst? 
