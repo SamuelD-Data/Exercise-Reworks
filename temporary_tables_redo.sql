2dfbf42ab9c4592a4d3c175801a9deec0a6c6ae4
Samuel Davila <samuelrdavila@gmail.com>
2020-08-31 12:19:23 -0500

    answered questions 1-3

--- temporary_tables_redo.sql
+++ temporary_tables_redo.sql
@@ -1,7 +1,20 @@
 -- Using the example from the lesson, re-create the employees_with_departments table.
 
+CREATE TEMPORARY TABLE employees_with_departments AS
+SELECT emp_no, first_name, last_name, dept_no, dept_name
+FROM employees.employees
+JOIN employees.dept_emp USING(emp_no)
+JOIN employees.departments USING(dept_no);
+
 --  Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
+
+ALTER TABLE employees_with_departments ADD full_name VARCHAR(64);
+
 -- Update the table so that full name column contains the correct data
+
+UPDATE employees_with_departments
+SET full_name = CONCAT(first_name, ' ', last_name);
+
 -- Remove the first_name and last_name columns from the table.
 
 -- What is another way you could have ended up with this same table?
